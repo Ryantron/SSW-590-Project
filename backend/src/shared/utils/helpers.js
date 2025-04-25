7bef775e20bb9d274d6a81c213500895744e1a21
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2025-04-25 18:52:07 -0400

    refactored joinPath

--- backend/src/shared/utils/helpers.js
+++ backend/src/shared/utils/helpers.js
@@ -1,7 +1,6 @@
 import path from 'path';
 
 export function joinPath(...paths) {
-  const pth = path.join(...paths);
-  const posixpath = pth.replaceAll(path.sep, path.posix.sep);
-  return posixpath;
+  const pth = path.posix.join(...paths);
+  return pth;
 }
